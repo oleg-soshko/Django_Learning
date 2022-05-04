0d92c92070efe3d940623a360f2421f6659f316f
feispy <oleffkaaa@gmail.com>
2022-05-04 16:06:23 +0300

    04.05 template

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import index, test
+from .views import index
 
 
 urlpatterns = [
     path('', index),
-    path('test/', test),
 ]
--- news/views.py
+++ news/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .models import News
+
+
 def index(request):
-    return HttpResponse('Hello world!')
-
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', context)
