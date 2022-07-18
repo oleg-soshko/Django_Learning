26846e08679cde4e0a199685d0adb126479aa07a
feispy <oleffkaaa@gmail.com>
2022-07-18 14:34:46 +0300

    add forms

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, get_category, view_news
+from .views import index, get_category, view_news, add_news
 
 
 urlpatterns = [
     path('', index, name='home'),
     path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/add-news/', add_news, name='add_news'),
 ]
