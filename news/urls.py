e08115c03db454603604c8a331610f00bba625d0
feispy <oleffkaaa@gmail.com>
2022-07-18 16:34:54 +0300

    add validate

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, get_category, view_news, add_news
+from .views import index, get_category, view_news, add_news, HomeNews
 
 
 urlpatterns = [
-    path('', index, name='home'),
+    # path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:news_id>/', view_news, name='view_news'),
     path('news/add-news/', add_news, name='add_news'),
--- news/views.py
+++ news/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 
 
 from .models import News, Category
 from .forms import NewsForm
+
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    # extra_context = {'title': 'Главная'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
 
 
 def index(request):
@@ -22,16 +35,34 @@ def get_category(request, category_id):
 
 
 def view_news(request, news_id):
-    #news_item = News.objects.get(pk=news_id)
+    # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
+# С несвязаной формой с моделью
+'''
 def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
+            # print(form.cleaned_data)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
+'''
+
+
+# С связаной формой с моделью
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = form.save()
+            return redirect(news)
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
+
