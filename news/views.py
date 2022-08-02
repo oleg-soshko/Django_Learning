049240b46ecd591b47eeb7457c8f818aadc41942
feispy <oleffkaaa@gmail.com>
2022-08-02 15:54:12 +0300

    02.08

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.db.models import Count
 
 
 from .models import News, Category
